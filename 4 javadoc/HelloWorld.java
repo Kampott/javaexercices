be527293978561b919aef5de54144d39911489e1
Kampott <anis.bouslama.ru@gmail.com>
2024-11-30 16:24:35 +0300

    javadoc

---
+++ 4 javadoc/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * Класс HelloWorld демонстрирует базовый пример использования утилиты javadoc.
+ * 
+ * @author Anis Bouslama
+ * @version 1.0
+ * @since 2024-11-30
+ */
+public class HelloWorld {
+
+    /**
+     * Основной метод программы.
+     * Выводит приветственное сообщение в консоль.
+     * 
+     * @param args массив строковых аргументов командной строки
+     */
+    public static void main(String[] args) {
+        // Вывод приветственного сообщения
+        System.out.println("Привет, мир!");
+    }
+}
